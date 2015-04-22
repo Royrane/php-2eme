c00ac2faae656c3e620196d8e6f8e2460d43458e
Oriane Martin <orianemeuh@gmail.com>
2015-04-22 19:14:44 +0200

    Test
    essaie des exercices, pas tous réussis

---
+++ chapitre02/get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" >
+        <title>Exercice 3</title>
+    </head>
+    <body>
+        <p> <a href="traite.php?nom=Charles">Appuyez ici</a> pour transmettre le nom</p>
+
+    </body>
+</html>
+
