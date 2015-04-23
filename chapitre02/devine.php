1c70bc58206ffdef3de57e3c9eec351a8a89a427
Oriane Martin <oriane.martin@cpnv.ch>
2015-04-23 09:37:40 +0200

    yolo

--- chapitre02/devine.php
+++ chapitre02/devine.php
@@ -1,3 +1,9 @@
+<?php
+CONST NBR_MAX = 5;
+CONST NBR_MIN = 0;
+$nombre = rand(NBR_MIN, NBR_MAX);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -7,9 +13,9 @@
     <body>
         <h1>Jeu de devinette</h1>
         <form action="resultat.php" method="post">
-            <label>Deviner le nombre entre 0 et 5<br> <input type="text" name="chiffre" size="40" maxlength="2" /></label>
+            <label>Deviner le nombre entre 0 et 5<br> <input type="number" name="chiffre" size="40" min ="0" max="5" maxlength="1" /></label>
             <input type="submit" name="test" value="Tester" />
-            <input type="hidden" name="chiffreadeviner" value="3"/>
+            <input type="hidden" name="chiffreadeviner" value="<?=$nombre?>"/>
         </form>
     </body>
 </html>
--- chapitre02/resultat.php
+++ chapitre02/resultat.php
@@ -1,19 +1,25 @@
 <?php
-    $chiffre = $_POST['chiffre'];//definir les variables
-    $chiffre_a_deviner = $_POST['chiffreadeviner'];
+    $chiffre = (int)$_POST['chiffre'];//definir les variables
+    $chiffre_a_deviner = (int)$_POST['chiffreadeviner'];
+   
+   // Issu de la correction avec le professeur 
+    $gagne = $chiffre == $chiffre_a_deviner;
+    if ($gagne) {$titre = "Vous avez gagné!";} else {$titre = "Vous avez perdu!";}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Exercice 2-002</title>
+  <title><?php echo $titre ?></title>
 </head>
 <body>
 
-<h3>Voici les valeurs transmises par l'utilisateur</h3>
+<h3>TADAAAAAAM</h3>
 
 <?php
- if($chiffre===$chiffre_a_deviner){ echo "Félicitations, tu as réussi à deviner le nombre!";} else {echo '<a href="javascript:history.back()">Réessayer!</a>';};
+ if(/*$chiffre===$chiffre_a_deviner*/$gagne){ 
+     echo "Félicitations, tu as réussi à deviner le nombre!";} else{
+     echo '<a href="javascript:history.back()">Tu as perdu. Réessayer?</a>';}
 ?>
 
 </body>
